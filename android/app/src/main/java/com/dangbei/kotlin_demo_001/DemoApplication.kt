dcd3db1cee716737c5b493729c0b0b78c845afb5
xuzhili <xuzhili@znds.com>
2020-08-22 10:25:44 +0800

    feature:init

---
+++ android/app/src/main/java/com/dangbei/kotlin_demo_001/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.dangbei.kotlin_demo_001
+
+import io.flutter.app.FlutterApplication
+
+class DemoApplication : FlutterApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ android/app/src/main/java/com/dangbei/kotlin_demo_001/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dangbei.kotlin_demo_001
+
+import android.app.Activity
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+    }
+
+}
